68e09e41a2939e53b9e7b9225af50f332c7b94bd
Koichi.Shimomura <unieye51@gmail.com>
2021-11-22 11:25:34 +0900

    section-019: ボタンコンポーネントを作る

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <ButtonSample />
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import ButtonSample from "./components/buttons/ButtonSample.vue";
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    ButtonSample,
+  }
+});
+</script>
