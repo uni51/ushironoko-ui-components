87b536e59e173421e3bd4fe21b5b2f7330667436
Koichi.Shimomura <unieye51@gmail.com>
2021-11-22 11:52:30 +0900

    section-020: ダイアログコンポーネントを作る

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
-  <ButtonSample />
+  <div class="m-4">
+    <ButtonSample />
+    <DialogSample />
+  </div>
 </template>
 
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import ButtonSample from "./components/buttons/ButtonSample.vue";
+import DialogSample from "./components/dialog/DialogSample.vue";
 
 export default defineComponent({
   name: 'App',
   components: {
     ButtonSample,
-  }
+    DialogSample,
+}
 });
 </script>
